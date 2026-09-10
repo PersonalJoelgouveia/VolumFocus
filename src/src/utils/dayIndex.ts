d910ea220729435885944d7ee965f75001603295
PersonalJoelgouveia <personaljoelgouveia@gmail.com>
2026-09-09 22:28:00 -0300

    delete_XofaMistake

--- src/src/utils/dayIndex.ts
+++
@@ -1,4 +0,0 @@
-/** Índice de hoje na convenção do app (0=Segunda...6=Domingo). `Date.getDay()` usa 0=Domingo, por isso o deslocamento. */
-export function getTodayDayIndex(): number {
-  return (new Date().getDay() + 6) % 7;
-}
